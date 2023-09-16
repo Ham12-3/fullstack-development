0bcf74ad9f33d341e1d48394d6f619f7aeb1cdf2
Abdulhamid <mobolaji2309@gmail.com>
2023-09-16 11:21:24 +0100

    updaitng

--- space-X/js/script.js
+++ space-X/js/script.js
@@ -2,9 +2,14 @@ const btn = document.getElementById("menu-btn");
 const overlay = document.getElementById("overlay");
 btn.addEventListener("click", navToggle);
 const menu = document.getElementById("mobile-menu");
+const counters = document.querySelectorAll();
 function navToggle() {
   btn.classList.toggle("open");
   overlay.classList.toggle("overlay-show");
   document.body.classList.toggle("stop-scrolling");
   menu.classList.toggle("show-menu");
 }
+
+function countUp() {}
+
+countUp();
