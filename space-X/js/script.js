6e785df2533e66870589f0b13c6643dc12795f25
Abdulhamid <mobolaji2309@gmail.com>
2023-09-16 08:16:18 +0100

    updating

--- space-X/js/script.js
+++ space-X/js/script.js
@@ -1,7 +1,10 @@
 const btn = document.getElementById("menu-btn");
-
+const overlay = document.getElementById("overlay");
 btn.addEventListener("click", navToggle);
-
+const menu = document.getElementById("mobile-menu");
 function navToggle() {
   btn.classList.toggle("open");
+  overlay.classList.toggle("overlay-show");
+  document.body.classList.toggle("stop-scrolling");
+  menu.classList.toggle("show-menu");
 }
