1657c33168f28272de71d901c6fc1bd6de402ddc
Abdulhamid <mobolaji2309@gmail.com>
2023-09-16 11:56:52 +0100

    updating

--- space-X/js/script.js
+++ space-X/js/script.js
@@ -1,8 +1,10 @@
+// const { count } = require("console");
+
 const btn = document.getElementById("menu-btn");
 const overlay = document.getElementById("overlay");
-btn.addEventListener("click", navToggle);
+
 const menu = document.getElementById("mobile-menu");
-const counters = document.querySelectorAll();
+const counters = document.querySelectorAll(".counter");
 function navToggle() {
   btn.classList.toggle("open");
   overlay.classList.toggle("overlay-show");
@@ -10,6 +12,39 @@ function navToggle() {
   menu.classList.toggle("show-menu");
 }
 
-function countUp() {}
+btn.addEventListener("click", navToggle);
+document.addEvenetListener("scroll", scrollPage);
 
+function scrollPage() {}
+function countUp() {
+  counters.forEach((counter) => {
+    counter.innerText = "0";
+
+    const updateCounter = () => {
+      // Get count target
+      const target = +counter.getAttribute("data-target");
+      // Get current counter value
+      const c = +counter.innerText;
+
+      // Create an increment
+      const increment = target / 100;
+
+      // If counter is less than target, add increment
+      if (c < target) {
+        // Round up and set counter value
+        counter.innerText = `${Math.ceil(c + increment)}`;
+
+        setTimeout(updateCounter, 75);
+      } else {
+        counter.innerText = target;
+      }
+    };
+
+    updateCounter();
+  });
+}
 countUp();
+
+function reset() {
+  counters.forEach((counter) => (counter.innerHTML = "0"));
+}
