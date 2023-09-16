a8775fda52d0bce99c034711fd764cc77359615a
Abdulhamid <mobolaji2309@gmail.com>
2023-09-16 12:28:38 +0100

    updaitng

--- space-X/js/script.js
+++ space-X/js/script.js
@@ -5,6 +5,7 @@ const overlay = document.getElementById("overlay");
 
 const menu = document.getElementById("mobile-menu");
 const counters = document.querySelectorAll(".counter");
+let scrollStarted = false;
 function navToggle() {
   btn.classList.toggle("open");
   overlay.classList.toggle("overlay-show");
@@ -13,9 +14,18 @@ function navToggle() {
 }
 
 btn.addEventListener("click", navToggle);
-document.addEvenetListener("scroll", scrollPage);
+document.addEventListener("scroll", scrollPage);
 
-function scrollPage() {}
+function scrollPage() {
+  const scrollPos = window.scrollY;
+  if (scrollPos > 100 && !scrollStarted) {
+    countUp();
+    scrollStarted = true;
+  } else if (scrollPos < 100 && scrollStarted) {
+    reset();
+    scrollstarted = false;
+  }
+}
 function countUp() {
   counters.forEach((counter) => {
     counter.innerText = "0";
@@ -43,7 +53,6 @@ function countUp() {
     updateCounter();
   });
 }
-countUp();
 
 function reset() {
   counters.forEach((counter) => (counter.innerHTML = "0"));
