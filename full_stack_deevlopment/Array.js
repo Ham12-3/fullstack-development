5ff6e6b0ebb4b1d110de11829c7b160abd857f05
Ham12-3 <mobolaji2309@gmail.com>
2023-12-29 10:12:50 +0000

    'update'

---
+++ full_stack_deevlopment/Array.js
@@ -0,0 +1,11 @@
+// Using Push methods
+
+const fruits = ["Apple", "Banana"];
+
+const newArrayOfFruits = fruits.push("Kiwi");
+
+console.log(newArrayOfFruits);
+
+console.log("modified fruits", fruits);
+
+// Using pop and shift methods
