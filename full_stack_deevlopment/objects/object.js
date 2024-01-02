0a9ff858166dd2dbdfacfef54e14c91c9138849b
Ham12-3 <mobolaji2309@gmail.com>
2024-01-02 07:22:59 +0000

    'update'

---
+++ full_stack_deevlopment/objects/object.js
@@ -0,0 +1,42 @@
+// ----
+// Creating Object using Object literals
+// -----
+
+// Define an object to represent a book in a bookstore
+
+const boook = {
+  title: "Harry Potter",
+  author: "J.K Rowling",
+  available: true,
+  price: 9.99,
+};
+console.log(boook);
+
+// title
+// author
+// price
+// available
+// pages
+
+// Dot Notation
+
+const bookTitle = boook.title;
+console.log(bookTitle);
+
+// Bracket Notation
+
+const bookTitle2 = boook["title"];
+
+// Propeties with special characters
+
+const person = {
+  "first name": "ben",
+  "last name": "Joe",
+};
+
+console.log(person["first name"]);
+
+// Adding properties to objects
+
+objectname.newPropertyName = newValue;
+objectName["newPropertyName"] = newValue;
