422509f411c2ff2b977937a20255e427c061b9a9
Ham12-3 <mobolaji2309@gmail.com>
2024-01-20 10:01:26 +0000

    'update'

---
+++ full_stack_deevlopment/ARROW_FUNCTIONS/arrow_function.js
@@ -0,0 +1,16 @@
+const functionName = (param1, param2, param3) => XPathExpression;
+
+const functionName1 = () => ({ key: "value" });
+
+//
+// ARROW FUNCTION
+
+// Basic Syntax
+
+function multiply(a, b) {
+  return a * b;
+}
+
+const multiply = (a, b) => a * b;
+
+const result = multiply(2, 3);
