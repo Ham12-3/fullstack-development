6a864fdd4d5c87b77006eeadd643b16cf3d01f7f
Abdulhamid Sonaike <mobolaji2309@gmail.com>
2024-01-22 11:11:39 +0000

    update

--- full_stack_deevlopment/ARROW_FUNCTIONS/arrow_function_2.js
+++ full_stack_deevlopment/ARROW_FUNCTIONS/arrow_function_2.js
@@ -16,3 +16,38 @@ const updateStudents = students.map((student, index) => {
 });
 
 console.log(updateStudents);
+
+// Search and find filter using  find()
+
+// Finding the first patient with a specifi aliment in a hospital
+
+const patients = [
+  { id: 101, name: "Sarah", disease: "Cold" },
+  { id: 102, name: "Mike", disease: "Fever" },
+  { id: 3, name: "Lucy", disease: "Cold" },
+];
+// Uing ths find()
+
+const patientWithCold = patients.find((patient) => patient.disease === "Cold");
+
+// Search and filter using some()
+
+const patients2 = [
+  { id: 101, name: "Sarah", disease: "Cold" },
+  { id: 102, name: "Mike", disease: "Fever" },
+  { id: 3, name: "Lucy", disease: "Cold" },
+];
+
+const patientWithCold2 = patients2.some(
+  (patient) => patient.disease === "Cold"
+);
+
+// using every()
+
+const studentsArr = [
+  { id: 1, name: "Bob", grade: "A" },
+  { id: 2, name: "Alice", grade: "B" },
+  { id: 3, name: "Emma", grade: "C" },
+];
+
+const allPassed = studentsArr.every((student) => student.grade !== "F");
