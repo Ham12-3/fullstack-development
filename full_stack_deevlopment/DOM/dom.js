2bbc061dfb599164dee4200f89cf1182b83d14cc
Abdulhamid Sonaike <mobolaji2309@gmail.com>
2024-01-22 13:19:02 +0000

    updating the code

---
+++ full_stack_deevlopment/DOM/dom.js
@@ -0,0 +1,24 @@
+// Browser window object
+// ------
+// Window Object
+// --------
+// console.log(window.location.host);
+
+// console.log(window.history);
+// console.log(window.navigator);
+// console.log(window.screen);
+
+// window.prompt();
+// window.alert();
+// window.confirm();
+// window.open("https://ham12-portfolio.vercel.app");
+
+// console.log(document);
+
+// GetElementById method
+
+const conferenceTitleEl = document.getElementById("conferenceTitle");
+const conferenceDateEl = document.getElementById("conferenceDate");
+
+const locationTitleEl = document.getElementById("locationTitle");
+const conferenceLocationEl = document.getElementById("conferenceLocation");
