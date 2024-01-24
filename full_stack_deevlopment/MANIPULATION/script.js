837e04d947152f1b49c64b57d855bc9cdb266ad3
Abdulhamid Sonaike <mobolaji2309@gmail.com>
2024-01-24 13:08:28 +0000

    upating the code

---
+++ full_stack_deevlopment/MANIPULATION/script.js
@@ -0,0 +1,8 @@
+// Using tectContent to update the blog title 
+
+const blogTitleEl = document.getElementById("title")
+
+blogTitleEl.textContent = "New Blog Title"
+
+
+// Change the background color 
