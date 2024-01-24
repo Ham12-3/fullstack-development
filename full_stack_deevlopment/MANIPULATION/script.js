3c13c3526aa376398865672d2821aaca313940bf
Abdulhamid Sonaike <mobolaji2309@gmail.com>
2024-01-24 18:30:32 +0000

    updating the code

--- full_stack_deevlopment/MANIPULATION/script.js
+++ full_stack_deevlopment/MANIPULATION/script.js
@@ -1,8 +1,48 @@
-// Using tectContent to update the blog title 
+// Using tectContent to update the blog title
 
-const blogTitleEl = document.getElementById("title")
+const blogTitleEl = document.getElementById("title");
 
-blogTitleEl.textContent = "New Blog Title"
+// blogTitleEl.textContent = "New Blog Title"
 
+// Change the background color
 
-// Change the background color 
+const paragraphEl = document.getElementById("paragraph");
+
+paragraphEl.innerHTML = "<h3>I have changed</h3>";
+
+// using innerText to modify the comment section
+
+const firstCommentEl = document
+  .getElementById("comment1")
+  .querySelector(".commentText");
+
+// Using setAttribute
+
+const product1ImageEl = document
+  .getElementById("product1")
+  .querySelector("img");
+
+product1ImageEl.setAttribute("src", "new Image");
+
+// using setAttributes to change product links
+
+const product1linkEl = document.getElementById("product1").querySelector("a");
+
+product1linkEl.setAttribute("href", "new.html");
+
+// Using the getAttributes
+
+const imagesEl = document.getElementById("gallery").querySelectorAll("img");
+
+let imagesByAlice = [];
+
+// Using getAttributes to filter images by author Alice
+imagesEl.forEach((img) => {
+  const author = img.getAttribute("style");
+
+  if (author === "Alice") {
+    imagesByAlice.push(img);
+  }
+});
+
+imagesByAlice.forEach((img) => console.log(img));
