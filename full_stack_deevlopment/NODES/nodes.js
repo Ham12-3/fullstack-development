cef66718f28aec21b3623a63e12544bc91303e1c
Abdulhamid Sonaike <mobolaji2309@gmail.com>
2024-01-24 10:28:52 +0000

    update

--- full_stack_deevlopment/NODES/nodes.js
+++ full_stack_deevlopment/NODES/nodes.js
@@ -12,3 +12,31 @@ function collectData() {
         }
     }
 }
+
+// log all element with the name email 
+
+const emailEl = document.getElementsByName("email")
+
+console.log(emailEl)
+
+
+
+// Parent and child elements  
+
+// Parent element: parentNode,paretnElement 
+// child elements: firstChild, lastChild, firstElementChild,lastElementChild 
+
+
+// Accessing parent elements 
+
+const parentEl= document.getElementById("parent")
+
+const grandPrentEl = parentEl.parentNode;
+
+// Accessing the child elements 
+
+const firstChildEl = parent.firstChild
+
+const lastChildEl = parent.lastChild
+
+console.log(firstChildEl)
