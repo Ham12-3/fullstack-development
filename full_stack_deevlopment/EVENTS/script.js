dd5fcf6b7b757d0e3af6624010b6daa4e39655a2
Abdulhamid Sonaike <mobolaji2309@gmail.com>
2024-01-25 14:32:40 +0000

    updating the code

---
+++ full_stack_deevlopment/EVENTS/script.js
@@ -0,0 +1,42 @@
+// Common Event Types
+// 1. DOM events
+// 2. Mouse events
+// 3. Form events
+// 4. Linked to specific interactions or triggers
+// 5. keyboard Events
+
+function clickHandler() {
+  console.log("Click event is called");
+}
+
+function dbClickHandler() {
+  console.log("dbClickHanler event is called");
+}
+
+function onMouseOverHandler() {
+  console.log("önMouseOverHandler event is called");
+}
+
+function onMouseOutHandler() {
+  console.log("önMouseOutHandler event is called");
+}
+
+function onkeydownHandler() {
+  console.log("onkeydownHandler event is called");
+}
+
+function onkeyupHandler() {
+  console.log("onkeyupHandler event is called");
+}
+
+function onkeypressHandler() {
+  console.log("onkeypressHandler event is called");
+}
+
+function onfocusHandler() {
+  console.log("onfocusHandler event is called");
+}
+
+function onblurHandler() {
+  console.log("onblurHandler event is called");
+}
