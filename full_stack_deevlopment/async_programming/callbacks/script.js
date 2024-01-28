80128324cf06f0a0e9aea1f7a87a114c507b16b5
Abdulhamid Sonaike <mobolaji2309@gmail.com>
2024-01-28 15:55:36 +0000

    updating the codebase

--- full_stack_deevlopment/async_programming/callbacks/script.js
+++ full_stack_deevlopment/async_programming/callbacks/script.js
@@ -16,3 +16,27 @@ function logMessage(callback) {
 logMessage(() => {
   return "Hello this is a simple callback function";
 });
+
+// passing parameters to callback functions
+
+const logMessageHOF = (callback) => {
+  const result = callback("Agnes", "Appiah");
+
+  console.log(result);
+};
+
+const logMessageCallbackFn = (firstName, lastName) => {
+  return `hello ${firstName} ${lastName} this is a simple cb`;
+};
+
+logMessageHOF(logMessageCallbackFn);
+
+// HOF with parameter and call back function
+
+const calculateSumHOF = (a, b, callback) => {
+  const result = a + b;
+  callback(result);
+};
+calculateSumHOF(2, 3, (result) => {
+  console.log(result);
+});
