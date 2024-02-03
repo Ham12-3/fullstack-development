c7c09e426d310957857dcc0989f65614f32a911d
Abdulhamid Sonaike <mobolaji2309@gmail.com>
2024-02-03 10:12:15 +0000

    updating the XMLHTTP

--- full_stack_deevlopment/AJAX/script.js
+++ full_stack_deevlopment/AJAX/script.js
@@ -9,3 +9,22 @@
 // AJAX is a misleading name. AJAX applications might use XML to transport data, but it is equally common to transport data as plain text or JSON text.
 // AJAX allows web pages to be updated asynchronously by exchanging data with a web server behind the scenes. This means that it is possible to update parts of a web page, without reloading the whole page.
 //
+
+// Path: full_stack_deevlopment/AJAX/script.js
+
+const url = "https://jsonplaceholder.typicode.com/posts";
+
+// Create a new instance of XMLHttpRequest object
+
+const xhr = new XMLHttpRequest();
+// Configure the request using the open method
+xhr.open("GET", url, true);
+
+// Attach an event listener to the xhr object to listen for the load event. This event is triggered when the request is complete. The callback function will be called when the event is triggered.
+
+xhr.onreadystatechange = function () {
+  // Check if the request is complete {readyState 4} and successful(status 200)
+  if (xhr.readyState === 4 && xhr.status === 200) {
+    console.log(xhr.responseText);
+  }
+};
