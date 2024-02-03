0a0f50aedb363cfa4206becd2f9db7d391531860
Abdulhamid Sonaike <mobolaji2309@gmail.com>
2024-02-03 09:56:11 +0000

    updating the comments

---
+++ full_stack_deevlopment/AJAX/script.js
@@ -0,0 +1,11 @@
+// AJAX
+// help me pull the data and place it inside our application without having to refresh the page
+// AJAX is a technique for accessing web servers from a web page
+// AJAX stands for Asynchronous JavaScript And XML
+// AJAX is not a programming language
+// AJAX just uses a combination of:
+// A browser built-in XMLHttpRequest object (to request data from a web server)
+// JavaScript and HTML DOM (to display or use the data)
+// AJAX is a misleading name. AJAX applications might use XML to transport data, but it is equally common to transport data as plain text or JSON text.
+// AJAX allows web pages to be updated asynchronously by exchanging data with a web server behind the scenes. This means that it is possible to update parts of a web page, without reloading the whole page.
+//
