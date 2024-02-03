b8ecf4215c5499bd9310cec1bd8ca68177252b22
Abdulhamid Sonaike <mobolaji2309@gmail.com>
2024-02-03 11:10:33 +0000

    updating the codebase

--- full_stack_deevlopment/AJAX/script.js
+++ full_stack_deevlopment/AJAX/script.js
@@ -25,6 +25,75 @@ xhr.open("GET", url, true);
 xhr.onreadystatechange = function () {
   // Check if the request is complete {readyState 4} and successful(status 200)
   if (xhr.readyState === 4 && xhr.status === 200) {
-    console.log(xhr.responseText);
+    console.log(JSON.parse(xhr.responseText));
+  }
+
+  //   handle errors if any
+  if (xhr.readyState === 4 && xhr.status === 404) {
+    console.log("Error 404: Page not found");
+  }
+};
+
+// Send the request using the send method
+xhr.send();
+
+// using fetch API to make AJAX request
+// fetch(url)
+//   .then((response) => response.json())
+//   .then((data) => console.log(data))
+//   .catch((err) => console.log(err));
+
+// Fetch API
+// The Fetch API provides an interface for fetching resources (including across the network). It will seem familiar to anyone who has used XMLHttpRequest, but the new API provides a more powerful and flexible feature set.
+// The fetch() method takes one mandatory argument, the path to the resource you want to fetch. It returns a Promise that resolves to the Response to that request, whether it is successful or not.
+// You can also optionally pass in an init options object as the second argument (see Request).
+
+// The fetch() method takes one mandatory argument, the path to the resource you want to fetch. It returns a Promise that resolves to the Response to that request, whether it is successful or not.
+// You can also optionally pass in an init options object as the second argument (see Request).
+
+const fetchData = () => {
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+    .catch((err) => console.log(err));
+};
+
+fetchData();
+
+// using async await with fetch API
+
+// const fecthData2 = async () => {
+//   try {
+//     const response = await fetch(url);
+//     const data = await response.json();
+//     console.log(data);
+//   } catch (err) {
+//     console.log(err);
+//   }
+// };
+
+// fecthData2().then((res) => console.log(res));
+
+// using Axios to make AJAX request
+// Axios is a promise-based HTTP client for the browser and Node.js. It has a convenient and modern API simplifying asynchronous HTTP request and response handling.
+
+// Axios is a promise-based HTTP client for the browser and Node.js. It has a convenient and modern API simplifying asynchronous HTTP request and response handling.
+// Axios is not built into JavaScript like fetch, so you need to install it using npm or yarn before using it in your project.
+
+const fecthData2 = () => {
+  axios
+    .get(url)
+    .then((response) => console.log(response.data))
+    .catch((err) => console.log(err));
+};
+
+fecthData2();
+
+const fetchData3 = async () => {
+  try {
+    const { data } = await axios.get(url);
+    console.log(data);
+  } catch (err) {
+    console.log(err);
   }
 };
