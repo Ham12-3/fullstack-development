834d8ed4c1e57520ef10fbc1cf1a9a02515899d2
Abdulhamid Sonaike <mobolaji2309@gmail.com>
2024-02-04 21:46:07 +0000

    welcome to nodejs

---
+++ full_stack_deevlopment/nodejs/my-first-app/app.js
@@ -0,0 +1,6 @@
+// Nodejs guide : https://nodejs.org/en/docs/
+console.log("Hello World!");
+// Nodejs REPL : https://nodejs.org/en/docs/guides/getting-started-guide/
+// Nodejs guide : https://nodejs.org/en/docs/
+
+// Path: full_stack_deevlopment/nodejs/my-first-app/app.js
