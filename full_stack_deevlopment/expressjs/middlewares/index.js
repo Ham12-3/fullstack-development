493b535754575ed3f6fd6e4cda704a62311b8c28
Abdulhamid Sonaike <mobolaji2309@gmail.com>
2024-02-08 09:28:49 +0000

    expressjs routing

---
+++ full_stack_deevlopment/expressjs/middlewares/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+
+const app = express();  
+
+
+const PORT = 8082;
+
+
+
+// Start the server 
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+}
+);
