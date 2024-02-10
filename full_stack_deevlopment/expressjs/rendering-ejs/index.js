c38b103738a1f25e3796d4deaff7bd25d237c41e
Abdulhamid Sonaike <mobolaji2309@gmail.com>
2024-02-10 23:06:50 +0000

    updating the loops for ejs

--- full_stack_deevlopment/expressjs/rendering-ejs/index.js
+++ full_stack_deevlopment/expressjs/rendering-ejs/index.js
@@ -42,6 +42,40 @@ app.get("/user", (req, res) => {
   };
   res.render("userData.ejs", userData);
 });
+
+// render product to page/route
+app.get("/product", (req, res) => {
+  const products = {
+    products: [
+      {
+        id: 1,
+        name: "Product 1",
+        price: 100,
+      },
+      {
+        id: 2,
+        name: "Product 2",
+        price: 200,
+      },
+      {
+        id: 3,
+        name: "Product 3",
+        price: 300,
+      },
+      {
+        id: 4,
+        name: "Product 4",
+        price: 400,
+      },
+      {
+        id: 5,
+        name: "Product 5",
+        price: 500,
+      },
+    ],
+  };
+  res.render("products.ejs", products);
+});
 // Start the server
 
 app.listen(PORT, () => {
