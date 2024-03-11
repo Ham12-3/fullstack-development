2b58718c5bd2b9ff2e88197dde9d3084b767e291
Abdulhamid Sonaike <mobolaji2309@gmail.com>
2024-03-11 21:30:03 +0000

    updating the codebase

---
+++ python_practice/newscript.js
@@ -0,0 +1,12 @@
+for (let i = 0; i <= 5; i++) {
+  console.log("*".repeat(i));
+}
+
+console.log("------------------------ ");
+
+for (let i = 1; i < 3; i++) {
+  console.log("*".repeat(i));
+}
+for (let i = 3; i >= 1; i--) {
+  console.log("*".repeat(i));
+}
